bf962f3422f82538ee8f4857336d0133bb3a5db5
Irene <irene.iaccio@gmail.com>
2017-02-04 20:42:10 +0100

    base module

---
+++ README.md
@@ -0,0 +1,1 @@
+# Magento2 & Require Js. The right way #
---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+use \Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::MODULE,
+    'Nuovecode_RequireStuff',
+    __DIR__
+);
